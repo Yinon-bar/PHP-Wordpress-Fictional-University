cbba0a4cd1464302e94a60b2af8321afdf350228
Yinon Bar <yinonbar1988@gmail.com>
2023-03-11 15:17:21 +0200

    posts relations

--- themes/fictional-university-theme/archive-program.php
+++ themes/fictional-university-theme/archive-program.php
@@ -7,7 +7,7 @@ get_header(); ?>
   <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title">All Programs</h1>
     <div class="page-banner__intro">
-      <p>There is somthing for everyone</p>
+      <p>There is something for everyone</p>
     </div>
   </div>
 </div>
